f4125496b0a301b562311e9c580448af14d386ea
lastvalkyrieime <lastvalkyrieime@gmail.com>
2025-11-17 07:39:12 +0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const APPSCRIPT_URL = " https://lastvalkyrieime.github.io/lv-resource-shop/";
-
-// fetch products
-async function loadProducts(){
-  const res = await fetch(APPSCRIPT_URL + "?action=getProducts");
-  const data = await res.json();
-  document.getElementById("app").innerHTML = JSON.stringify(data);
-}
-loadProducts();
